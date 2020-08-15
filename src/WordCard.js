4d9bb4b7f308c985528e472e9eca1fd7bfa4f540
NadeeyaBY <57823801+NadeeyaBY@users.noreply.github.com>
2020-08-15 18:12:18 +0700

    Update WordCard1

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,7 +1,36 @@
 import React , { useState } from 'react';
 import _, { attempt } from 'lodash';
 import CharacterCard from './CharacterCard';
+export default function WordCard(props){
 
+    const [state, setState]= useState(prepareStateFromWord(props.value))
+
+    const activationHandler = c => {
+        console.log(`${c} has been activated.`)
+
+        let guess = state.guess + c
+        setState({...state, guess})
+        
+        if(guess.length == state.word.length){
+            if (guess == state.word){
+                console.log('yeah!')
+                setState({...state, completed: true})
+            }else{
+                console.log('reset')
+                    setState({...state, guess: '', attempt: state.attempt + 1})
+                }
+            }
+        }
+      
+    return (
+        <div>
+            { 
+                state.chars.map((c, i) =>
+                <CharacterCard value={c} key={i} activationHandler={activationHandler}/>) 
+            }
+        </div>
+    );   
+}
 
 const prepareStateFromWord = given_word => {
     let word = given_word.toUpperCase()
@@ -15,38 +44,4 @@ const prepareStateFromWord = given_word => {
 
     }
 
-}
-
-
-export default function WordCard(props){
-
-    const [state, setState]= useState(prepareStateFromWord(props.value))
-
-    const activationHandler = c => {
-        console.log(`${c} has been activated.`)
-
-        let guess = state.guess + c
-        setState({...state, guess})
-        
-        if(guess.length == state.word.length){
-            if(guess == state.word){
-                console.log('yeah!')
-                setState({...state, guess: '', completed: true})
-            }else{
-                console.log('reset')
-                    setState({...state, guess: '', attempt: state.attempt + 1})
-                }
-            }
-        }
-      
-    
-
-    return (
-        <div>
-            { 
-                state.chars.map((c, i) =>
-                <CharacterCard value={c} key={i} activationHandler={activationHandler}/>) 
-            }
-        </div>
-    );   
 }
