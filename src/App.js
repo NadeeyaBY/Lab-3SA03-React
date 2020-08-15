277a383507ab59ea7471d84864338f6505a73161
NadeeyaBY <57823801+NadeeyaBY@users.noreply.github.com>
2020-08-15 16:33:06 +0700

    part8.2

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 import CharacterCard from './CharacterCard';
 import WordCard from './WordCard.js';
 import './App.css';
-
-
 const word = "Hello";
 function App() {
   return (
-    <div >
-     <WordCard value="hello"/>
+    <div>
+      <WordCard value="hello" />
 
     </div>
   );
 }
-
 export default App;
