97c5389eda3628e0622caf6310739353837984b2
NadeeyaBY <57823801+NadeeyaBY@users.noreply.github.com>
2020-08-30 17:53:11 +0700

    commit #2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
 
 import './App.css';
-const word = "Hello";
+//const word = "Hello";
+
+var word = "  "; 
+
+
+var rand = Math.floor(Math.random() * 5);
+switch (rand) {
+  case 0:  
+    word = "apple";
+    break;
+  case 1: 
+    word = "banana";
+    break;
+  case 2:  
+    word = "pineapple";
+    break;
+  case 3:      
+    word = "fruit";
+    break;
+  case 4:      
+    word = "lemon";
+    break;  
+}
+
 function App() {
   return (
     <div>
